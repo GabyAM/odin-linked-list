e39aa793c0519aa7979230d65748988003fdac36
GabyAM <gdgabyam@gmail.com>
2023-12-04 11:12:34 -0300

    Adds LinkedList class with basic functions
    This list can add nodes at the start and at the end of it. There is also a function to print the list.

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
+class LinkedList {
+	constructor() {
+		this.last = null;
+		this.first = null;
+	}
+
+	append(value) {
+		const newNode = new Node(value);
+		if (this.last === null) {
+			this.first = newNode;
+		} else {
+			this.last.next = newNode;
+		}
+		this.last = newNode;
+	}
+
+	prepend(value) {
+		const newNode = new Node(value);
+		newNode.next = this.first;
+		this.first = newNode;
+	}
+	toString() {
+		let string = "";
+		let item = this.first;
+		while (item !== null) {
+			string += `(${item.value}) -> `;
+			item = item.next;
+		}
+		return (string += "null");
+	}
+}
+
 class Node {
 	constructor(value = null) {
 		this.value = value;
 		this.next = null;
 	}
 }
+// list.pop();
+// console.log(list.contains(3));
+// console.log(list.contains("end"));
+// console.log(list.find("start"));
+//list.insertAt(2, "middle!");
+//list.removeAt(4);
