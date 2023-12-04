b3e851d2dc15200c533ef2aba6054f351b857f71
GabyAM <gdgabyam@gmail.com>
2023-12-04 11:15:24 -0300

    adds more functions to LinkedList
    Utility functions were added to the LinkedList class, such as pop(), to remove the last item, at(), wich returns the node of the input node, insertAt(), wich uses the previously mentioned function to insert a node in the input index, and more.

--- index.js
+++ index.js
@@ -19,6 +19,78 @@ class LinkedList {
 		newNode.next = this.first;
 		this.first = newNode;
 	}
+
+	size() {
+		let item = this.first;
+		let count = 0;
+		while (item !== null) {
+			count += 1;
+			item = item.next;
+		}
+		return count;
+	}
+
+	head() {
+		return this.first;
+	}
+	tail() {
+		return this.last;
+	}
+	at(index, list = this.first) {
+		if (list === null) {
+			return null;
+		}
+		if (index === 0) {
+			return list;
+		} else {
+			index--;
+			return this.at(index, list.next);
+		}
+		// let item = this.first;
+		// while (index > 0 && item !== null) {
+		// 	item = item.next;
+		// 	index--;
+		// }
+		// return item;
+	}
+	pop() {
+		let item = this.first;
+		while (item.next !== this.last) {
+			item = item.next;
+		}
+		this.last = item;
+		this.last.next = null;
+	}
+	contains(value, list = this.first) {
+		//creates a copy of this.first at first iteration
+		if (list === null) {
+			return false;
+		} else {
+			if (list.value === value) {
+				return true;
+			}
+			return this.contains(value, list.next);
+		}
+	}
+	find(value) {
+		let item = this.first;
+		let index = 0;
+		while (item !== null && item.value !== value) {
+			item = item.next;
+			index++;
+		}
+		return item === null ? null : index;
+	}
+	insertAt(index, value) {
+		const newNode = new Node(value);
+		const previous = this.at(index - 1);
+		newNode.next = previous.next;
+		previous.next = newNode;
+	}
+	removeAt(index) {
+		const previous = this.at(index - 1);
+		previous.next = previous.next.next;
+	}
 	toString() {
 		let string = "";
 		let item = this.first;
@@ -36,7 +108,15 @@ class Node {
 		this.next = null;
 	}
 }
+
+const list = new LinkedList();
+list.append(1);
+list.append(2);
+list.append(3);
+list.prepend("start");
+list.append("end");
 // list.pop();
+// console.log(list.toString());
 // console.log(list.contains(3));
 // console.log(list.contains("end"));
 // console.log(list.find("start"));
